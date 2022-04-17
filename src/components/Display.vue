09462d5641fa8718e139011d45ea596c841ec2fc
huanmingchang <huanming1988@gmail.com>
2022-04-17 13:25:19 +0800

    feature: finish html structure and styles

---
+++ src/components/Display.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+div.text-slate-200.p-6.border.border-slate-500
+  div.text-lg
+    div.flex.cursor-pointer
+      span.font-bold nav
+      span.ml-1 [-]
+    div.pl-8
+      div.text-lg
+        div.flex.cursor-pointer
+          span.font-bold header
+          span.ml-1 [-]
+      div.pl-8
+        div.text-lg
+          div.inline
+            span.font-bold creator
+          div.inline
+            span.ml-1 :
+            span.ml-3.text-rose-300 3D Fabric Creator
+      div.pl-8
+        div.text-lg
+          div.inline
+            span.font-bold product
+          div.inline
+            span.ml-1 :
+            span.ml-3.text-rose-300 product
+</template>
---
+++ src/components/Input.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+div.border.border-slate-500
+  div.flex.justify-end.p-3
+    button.bg-slate-200.text-slate-800.p-2.font-bold ＋Add New Pair
+  div
+    div.px-5.py-2.flex
+      div.grid.grid-cols-2.gap-4.flex-grow.mr-3
+        input.bg-transparent.outline-none.border.text-slate-200.border-slate-500.text-base.px-2.py-1.w-full
+        input.bg-transparent.outline-none.border.text-slate-200.border-slate-500.text-base.px-2.py-1.w-full
+      button.w-10.text-sm.flex.justify-center.items-center.bg-slate-300.text-slate-800 －
+</template>
