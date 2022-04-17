8b960ee93393aa175655c4ab46eb8ac3573ff2ea
huanmingchang <huanming1988@gmail.com>
2022-04-17 15:44:26 +0800

    feat: install pinia feat: update component name

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 div.px-20.pb-20.pt-5.h-screen.bg-slate-900
   div.mb-2.text-2xl.text-slate-200.font-bold Nested Key-Value Pair Tree Viewer
   div.grid.grid-cols-2.gap-8.h-full
-    Input
-    Display
+    InputSection
+    DisplaySection
 </template>
 
 <script setup>
-import Input from './components/Input.vue'
-import Display from './components/Display.vue'
+import InputSection from './components/InputSection.vue'
+import DisplaySection from './components/DisplaySection.vue'
 </script>
