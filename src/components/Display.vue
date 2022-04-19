5ecc32a313d360347072ae577b32d206442f113b
huanmingchang <huanming1988@gmail.com>
2022-04-19 21:44:13 +0800

    feat: add data to DisplaySection feat: add toggle isOpen function

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,26 +1,24 @@
 <template lang="pug">
-div.text-slate-200.p-6.border.border-slate-500
-  div.text-lg
-    div.flex.cursor-pointer
-      span.font-bold nav
-      span.ml-1 [-]
-    div.pl-8
-      div.text-lg
-        div.flex.cursor-pointer
-          span.font-bold header
-          span.ml-1 [-]
-      div.pl-8
-        div.text-lg
-          div.inline
-            span.font-bold creator
-          div.inline
-            span.ml-1 :
-            span.ml-3.text-rose-300 3D Fabric Creator
-      div.pl-8
-        div.text-lg
-          div.inline
-            span.font-bold product
-          div.inline
-            span.ml-1 :
-            span.ml-3.text-rose-300 product
+div.text-lg
+  div.flex.cursor-pointer(v-if="item.children" @click="isOpen = !isOpen")
+    span.font-bold {{item.key}}
+    span.ml-1 {{isOpen ? '[-]' : '[+]'}}
+  div.text-lg(v-if="!item.children")
+    div.inline
+      span.font-bold {{item.key}}
+    div.inline
+      span.ml-1 :
+      span.ml-3.text-rose-300 {{item.value}} 
+  template(v-if="item.children")
+    Display.pl-8(v-show="isOpen" v-for="item in item.children" :key="item.id" :item="item" )
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  item: Object,
+})
+
+const isOpen = ref(true)
+</script>
