bc8e63b76f6413fcde825bf77d47d968ade2e63b
huanmingchang <huanming1988@gmail.com>
2022-04-20 07:50:18 +0800

    feat: finish init structure

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,11 +1,15 @@
 <template lang="pug">
-div.border.border-slate-500
-  div.flex.justify-end.p-3
-    button.bg-slate-200.text-slate-800.p-2.font-bold ＋Add New Pair
-  div
-    div.px-5.py-2.flex
-      div.grid.grid-cols-2.gap-4.flex-grow.mr-3
-        input.bg-transparent.outline-none.border.text-slate-200.border-slate-500.text-base.px-2.py-1.w-full
-        input.bg-transparent.outline-none.border.text-slate-200.border-slate-500.text-base.px-2.py-1.w-full
-      button.w-10.text-sm.flex.justify-center.items-center.bg-slate-300.text-slate-800 －
+div.px-5.py-2.flex
+  div.grid.grid-cols-2.gap-4.flex-grow.mr-3
+    input.bg-transparent.outline-none.border.text-slate-200.border-slate-500.text-base.px-2.py-1.w-full(:value="item.value ? '.' + item.key : item.key + '.'")
+    input.bg-transparent.outline-none.border.text-slate-200.border-slate-500.text-base.px-2.py-1.w-full(:value="item.value")
+  button.w-10.text-sm.flex.justify-center.items-center.bg-slate-300.text-slate-800 －
+template(v-if="item.children")
+  Input(v-for="item in item.children" :key="item.id" :item="item")
 </template>
+
+<script setup>
+const props = defineProps({
+  item: Object,
+})
+</script>
