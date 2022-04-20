968c84a683dcce6efd017e3f7c3bf8e15b865149
huanmingchang <huanming1988@gmail.com>
2022-04-20 15:24:19 +0800

    feat: update DisplaySection render function

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,21 +1,20 @@
 <template lang="pug">
 div.text-lg
-  div.flex.cursor-pointer(v-if="item.children" @click="isOpen = !isOpen")
-    span.font-bold {{item.key}}
+  div.flex.cursor-pointer(@click.stop.prevent="isOpen = !isOpen")
+    span.font-bold {{Object.keys(item)}}
     span.ml-1 {{isOpen ? '[-]' : '[+]'}}
-  div.text-lg(v-if="!item.children")
+  div.text-lg
     div.inline
-      span.font-bold {{item.key}}
+      span.font-bold {{Object.keys(item)}}
     div.inline
       span.ml-1 :
-      span.ml-3.text-rose-300 {{item.value}} 
-  template(v-if="item.children")
-    Display.pl-8(v-show="isOpen" v-for="item in item.children" :key="item.id" :item="item")
+      span.ml-3.text-rose-300 {{Object.values(item)}}
+  //- template(v-if="!Object.values(item)")
+  //-   Display.pl-8(v-show="isOpen" v-for="item in item" :key="item" :item="item")
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
 const props = defineProps({
   item: Object,
 })
--- src/components/DisplaySection.vue
+++ src/components/DisplaySection.vue
@@ -1,11 +1,10 @@
 <template lang="pug">
 div.text-slate-200.p-6.border.border-slate-500
-  Display(v-for="item in dataInDisplay" :key="item.id" :item="item")
+  Display(v-for="item in dataInDisplay" :key="item" :item="item")
 </template>
 
 <script setup>
 import Display from './Display.vue'
-// import { treeData } from '../stores/store'
-import { inject, computed, reactive } from 'vue'
+import { inject } from 'vue'
 const dataInDisplay = inject('treeData')
 </script>
