73749df24e9b2250e12cd86655cd79d5cc432b12
huanmingchang <huanming1988@gmail.com>
2022-04-24 16:24:36 +0800

    feat: finish display section

--- src/components/DisplaySection.vue
+++ src/components/DisplaySection.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 div.text-slate-200.p-6.border.border-slate-500
-  Display(v-for="item in Object.keys(dataInDisplay.root)" :key="item" :item="item" :data="dataInDisplay.root")
+  Display(v-for="item in Object.getOwnPropertyNames(dataInDisplay.root)" :key="item" :item="item" :data="dataInDisplay.root")
 </template>
 
 <script setup>
